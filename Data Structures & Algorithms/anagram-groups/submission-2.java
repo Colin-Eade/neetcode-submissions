9f216c1768ee80a9dde6866f601769bba7428d26
Colin Eade <145029406+Colin-Eade@users.noreply.github.com>
2026-09-10 11:52:54 -0400

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> anagramMap = new HashMap<>();
+
+        for (String str : strs) {
+            int[] letterCounts = new int[26];
+
+            for (char letter : str.toCharArray()) {
+                letterCounts[letter - 'a']++;
+            }
+            String key = Arrays.toString(letterCounts);
+            
+            if (!anagramMap.containsKey(key)) {
+                anagramMap.put(key, new ArrayList<>());
+            }
+            anagramMap.get(key).add(str);
+        }
+        return new ArrayList<>(anagramMap.values());
+    }
+}
